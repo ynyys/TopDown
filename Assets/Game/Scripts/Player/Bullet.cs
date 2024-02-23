e1ade142082bc37970a6938054bd2a98c32373fe
ynyys <oliverjones7806@gmail.com>
2024-02-23 22:42:18 +0000

    weapon can now kill enemies with varying health

--- Assets/Game/Scripts/Player/Bullet.cs
+++ Assets/Game/Scripts/Player/Bullet.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    // Variables
+    [SerializeField]
+    private float _damageAmount;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyMovement>())
         {
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
+            //Destroy(collision.gameObject);
+            //Destroy(gameObject);#
+            var healthController = collision.gameObject.GetComponent<HealthController>();
+
+            healthController.TakeDamage(_damageAmount);
         }
         Destroy(gameObject);
     }
